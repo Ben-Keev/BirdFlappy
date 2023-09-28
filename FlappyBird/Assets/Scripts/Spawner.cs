30c2e31bccb7079ee0213feb0859f86e065c0367
DKITguuy9 <D00254413@student.dkit.ie>
2023-09-28 03:59:09 +0100

    Make Obstacle into prefab

--- FlappyBird/Assets/Scripts/Spawner.cs
+++ FlappyBird/Assets/Scripts/Spawner.cs
@@ -5,29 +5,21 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public float queueTime = 1.5f;
-    public float time = 0;
-    // Game Object specified in unity
-    public GameObject obstacle;
-    public float height;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(time > queueTime) {
-            GameObject go = Instantiate(obstacle);
-            go.transform.position = transform.position+
-            new Vector3(0, Random.Range(-height, height), 0);
-            time = 0;
-            // Removes the object after 10 seconds
-            Destroy(go, 10);
-        }
-
-        time += Time.deltaTime;
-    }
-
+    private float time = 0;
+    public GameObject obstacle; 
+    public float height; 
+    
     // Update is called once per frame
     void Update()
     {
+        if(time>queueTime){
+            GameObject go = Instantiate(obstacle);
+            go.transform.position = transform.position+
+            new Vector3(0, Random.Range(-height, height),0);
+            time = 0;
+            Destroy(go,10);
+        }
+        time += Time.deltaTime;
         
     }
 }
