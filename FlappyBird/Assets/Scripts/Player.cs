9f8b0de0b4f5324118851316583c2e891452242b
DKITguuy9 <D00254413@student.dkit.ie>
2023-09-28 03:50:46 +0100

    Update Scripts from moodle

--- FlappyBird/Assets/Scripts/Player.cs
+++ FlappyBird/Assets/Scripts/Player.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{   
-    public float velocity = 2.4f;
-    private Rigidbody2D rigidbody;
-
+{
+    public GameManager gameManager;
+    public bool isDead = false; 
+    public float velocity = 2.4f; 
+    public Rigidbody2D rigidbody;
     public Animator anim;
-    
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+       rigidbody = GetComponent<Rigidbody2D>();  
     }
+    
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            rigidbody.velocity = Vector2.up * velocity;
+        rigidbody.velocity = Vector2.up * velocity; 
         }
-        
-    }
+}
+    private void OnCollisionEnter2D(Collision2D other) {
+            isDead = true; 
+            gameManager.GameOver(); 
+       
+}
 }
